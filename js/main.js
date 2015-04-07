72c246d8f4a523e1d1197968b863415a5868b9ce
Andrew_Dyachenko <cccp.world@gmail.com>
2015-04-07 23:18:59 +0600

    Галлерея

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$('#photo-gallery .item').nivoLightbox();
+
+	function equalHeight () {
+	    var biggestHeight = 0;
+	    $('#photo-gallery .item img').each(function () {
+	        if($(this).height() > biggestHeight) {
+	            biggestHeight = $(this).height();  
+	        }  
+	    });
+	    $('#photo-gallery .item').height(biggestHeight); 
+	}
+	equalHeight();
+	$(window).resize(function () {
+		$('#photo-gallery .item').removeAttr('style');
+		equalHeight();
+	});
+});
--- photo-gallery.php
+++ photo-gallery.php
@@ -4,40 +4,73 @@
 	<!-- Главная. / Main. -->
 	<main>
 		<div id="photo-gallery">
+			<h1>
+				Фотогалерея. Изделия из древесины
+			</h1>
 			<div class="row">
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-1.jpg" class="item" data-lightbox-gallery="gallery1" title="Столовые приборы">
+						<img src="/img/photo-gallery/image-1.jpg" alt="Cлайд-1" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-2.jpg" class="item" data-lightbox-gallery="gallery1" title="Куханная мебель">
+						<img src="/img/photo-gallery/image-2.jpg" alt="Cлайд-2" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-3.jpg" class="item" data-lightbox-gallery="gallery1" title="Направляющие колонки">
+						<img src="/img/photo-gallery/image-3.jpg" alt="Cлайд-3" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-4.jpg" class="item" data-lightbox-gallery="gallery1" title="Кухонные ёмкости">
+						<img src="/img/photo-gallery/image-4.jpg" alt="Cлайд-4" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-5.jpg" class="item" data-lightbox-gallery="gallery1" title="Велосипед">
+						<img src="/img/photo-gallery/image-5.jpg" alt="Cлайд-5" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-6.jpg" class="item" data-lightbox-gallery="gallery1" title="Полы">
+						<img src="/img/photo-gallery/image-6.jpg" alt="Cлайд-6" width="100%" height="100%">
 					</a>
 				</div>
 				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-					<a href="" class="item">
-						<img src="http://placehold.it/100x100.png" alt="" width="100%" height="100%">
+					<a href="/img/photo-gallery/image-7.jpg" class="item" data-lightbox-gallery="gallery1" title="Устройства хранения">
+						<img src="/img/photo-gallery/image-7.jpg" alt="Cлайд-7" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-8.jpg" class="item" data-lightbox-gallery="gallery1" title="Деревяные фигурки">
+						<img src="/img/photo-gallery/image-8.jpg" alt="Cлайд-8" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-9.jpg" class="item" data-lightbox-gallery="gallery1" title="Декоративные вставки">
+						<img src="/img/photo-gallery/image-9.jpg" alt="Cлайд-9" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-10.jpg" class="item" data-lightbox-gallery="gallery1" title="Будка"> 
+						<img src="/img/photo-gallery/image-10.jpg" alt="Cлайд-10" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-11.jpg" class="item" data-lightbox-gallery="gallery1" title="Интерьер">
+						<img src="/img/photo-gallery/image-11.jpg" alt="Cлайд-11" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-12.jpg" class="item" data-lightbox-gallery="gallery1" title="Деревяные модели">
+						<img src="/img/photo-gallery/image-12.jpg" alt="Cлайд-12" width="100%" height="100%">
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+					<a href="/img/photo-gallery/image-13.jpg" class="item" data-lightbox-gallery="gallery1" title="Дома">
+						<img src="/img/photo-gallery/image-13.jpg" alt="Cлайд-13" width="100%" height="100%">
 					</a>
 				</div>
 			</div>
