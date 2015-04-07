85c691dbbf0f773611979c115b6cd33a8edbef41
Andrew_Dyachenko <cccp.world@gmail.com>
2015-04-07 21:23:17 +0600

    Раздел №3

--- physical-and-mechanical-properties.php
+++ physical-and-mechanical-properties.php
@@ -53,6 +53,11 @@
 			Текстура пород древесины
 		</h2>
 		<table class="table table-bordered">
+			<caption>
+				<i>
+					Таблица 1.
+				</i>
+			</caption>
 			<thead>
 				<tr>
 					<th>Название древесины</th>
@@ -183,16 +188,571 @@
 			Средняя влажность в свежесрубленном состоянии, %
 		</h3>
 		<table class="table table-bordered">
+			<caption>
+				<i>
+					Таблица2.
+				</i>
+			</caption>
 			<thead>
 				<tr>
-					<th>Название древесины</th>
-					<th>Текстура</th>
+					<th>Порода</th>
+					<th class="text-center">%</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
-					<td>Акация белая</td>
-					<td>Полосы, кольца, тонкие линии</td>
+					<td>Ель</td>
+					<td class="text-center">91</td>
+				</tr>
+				<tr>
+					<td>Лиственница</td>
+					<td class="text-center">82</td>
+				</tr>
+				<tr>
+					<td>Пихта</td>
+					<td class="text-center">101</td>
+				</tr>
+				<tr>
+					<td>Сосна обыкновенная</td>
+					<td class="text-center">88</td>
+				</tr>
+				<tr>
+					<td>Сосна кедровая сибирская</td>
+					<td class="text-center">92</td>
+				</tr>
+				<tr>
+					<td>Липа мелколистная</td>
+					<td class="text-center">60</td>
+				</tr>
+				<tr>
+					<td>Осина</td>
+					<td class="text-center">82</td>
+				</tr>
+				<tr>
+					<td>Ольха</td>
+					<td class="text-center">84</td>
+				</tr>
+				<tr>
+					<td>Тополь</td>
+					<td class="text-center">93</td>
+				</tr>
+				<tr>
+					<td>Береза</td>
+					<td class="text-center">78</td>
+				</tr>
+				<tr>
+					<td>Бук</td>
+					<td class="text-center">64</td>
+				</tr>
+				<tr>
+					<td>Вяз</td>
+					<td class="text-center">78</td>
+				</tr>
+				<tr>
+					<td>Дуб</td>
+					<td class="text-center">50</td>
+				</tr>
+				<tr>
+					<td>Ясень обыкновенный</td>
+					<td class="text-center">36</td>
+				</tr>
+			</tbody>
+		</table>
+		<h2>
+			Усушка.
+		</h2>
+		<p>
+			Усушкой называется уменьшение линейных размеров и объема древесины при высыхании. Усушка начинается после полного удаления свободной влаги и с начала удаления связанной влаги.
+			Усушка по разным направлениям неодинакова. В среднем полная линейная усушка в тангентальном направлении составляет 6...10%, в радиальном - 3...5% и вдоль волокон - 0,1...0,3%.
+			Уменьшение объема древесины при испарении связанной влаги называется объемной усушкой.
+			При распиловке бревен на доски предусматривают припуски на усушку с тем, чтобы после высыхания пиломатериалы и заготовки имели заданные размеры.
+		</p>
+		<p>
+			<strong>
+				Усушка древесины (от водонасыщенного состояния до абсолютно сухого)
+			</strong>
+		</p>
+		<table class="table table-bordered">
+			<caption>
+				<i>
+					Таблица3.
+				</i>
+			</caption>
+			<thead>
+				<tr>
+					<th rowspan="2" class="vcenter text-center">
+						Вид древесины
+					</th>
+					<th colspan="3" class="text-center">
+						Усушка%
+					</th>
+				</tr>
+				<tr>
+					<th class="text-center">
+						В продольном направлении
+					</th>
+					<th class="text-center">
+						В тангенциальном направлении
+					</th>
+					<th class="text-center">
+						В радиальном направлении
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td>Афзелия</td>
+				<td class="text-center">0,2</td>
+				<td class="text-center">4-4,5</td>
+				<td class="text-center">2-3</td>
+			</tr>
+			<tr>
+				<td>Бальсовое дерево</td>
+				<td class="text-center">0,6</td>
+				<td class="text-center">3-5</td>
+				<td class="text-center">2-3</td>
+			</tr>
+			<tr>
+				<td>Береза</td>
+				<td class="text-center">0,6</td>
+				<td class="text-center">3-5</td>
+				<td class="text-center">2-3</td>
+			</tr>
+			<tr>
+				<td>Бук белый</td>
+				<td class="text-center">0,5</td>
+				<td class="text-center">10-12</td>
+				<td class="text-center">6-7</td>
+			</tr>
+			<tr>
+				<td>Бук лесной</td>
+				<td class="text-center">0,3</td>
+				<td class="text-center">8-12</td>
+				<td class="text-center">6-9</td>
+			</tr>
+			<tr>
+				<td>Вишня</td>
+				<td class="text-center">0,3</td>
+				<td class="text-center">7-8</td>
+				<td class="text-center">4-5</td>
+			</tr>
+			<tr>
+				<td>Вяз</td>
+				<td class="text-center">0,3</td>
+				<td class="text-center">8</td>
+				<td class="text-center">4,5</td>
+			</tr>
+			<tr>
+				<td>Груша</td>
+				<td class="text-center">0,4</td>
+				<td class="text-center">7-9</td>
+				<td class="text-center">4-5</td>
+			</tr>
+			<tr>
+				<td>Дуб</td>
+				<td class="text-center">0,4</td>
+				<td class="text-center">8-10</td>
+				<td class="text-center">4-5</td>
+			</tr>
+			<tr>
+				<td>Ель</td>
+				<td class="text-center">0,3</td>
+				<td class="text-center">6-8</td>
+				<td class="text-center">3-4</td>
+			</tr>
+			<tr>
+				<td>Клен</td>
+				<td class="text-center">0,5</td>
+				<td class="text-center">5-8</td>
+				<td class="text-center">3-4</td>
+			</tr>
+			<tr>
+				<td>Лимб</td>
+				<td class="text-center">0,2</td>
+				<td class="text-center">4,5-7,5</td>
+				<td class="text-center">3-6</td>
+			</tr>
+			<tr>
+				<td>Лиственница</td>
+				<td class="text-center">0,3</td>
+				<td class="text-center">7-8</td>
+				<td class="text-center">3-5</td>
+			</tr>
+			<tr>
+				<td>Орех</td>
+				<td class="text-center">0,5</td>
+				<td class="text-center">8-12</td>
+				<td class="text-center">5-6</td>
+			</tr>
+			<tr>
+				<td>Пихта</td>
+				<td class="text-center">0,1</td>
+				<td class="text-center">7-9</td>
+				<td class="text-center">3-4</td>
+			</tr>
+			<tr>
+				<td>Сосна (обычная)</td>
+				<td class="text-center">0,4</td>
+				<td class="text-center">6-8</td>
+				<td class="text-center">3-4</td>
+			</tr>
+			<tr>
+				<td>Сосна смолистая</td>
+				<td class="text-center">0,2</td>
+				<td class="text-center">7-7,5</td>
+				<td class="text-center">4-5</td>
+			</tr>
+			<tr>
+				<td>Тик</td>
+				<td class="text-center">0,4</td>
+				<td class="text-center">4,5-6</td>
+				<td class="text-center">2-3</td>
+			</tr>
+			<tr>
+				<td>Ясень</td>
+				<td class="text-center">0,2</td>
+				<td class="text-center">7-8</td>
+				<td class="text-center">4-5</td>
+			</tr>
+			</tbody>
+		</table>
+		<h3>
+			Внутренние напряжения
+		</h3>
+		<p>
+			Напряжения, которые возникают без участия внешних сил, называют внутренними. Причина образования напряжений при сушке древесины - неравномерность распределения влаги.
+			Если растягивающие напряжения достигнут предела прочности древесины на растяжение поперек волокон, то могут возникнуть трещины: в начале процесса сушки на поверхности сортимента, а в конце - внутри.
+			Внутренние напряжения сохраняются в высушенном материале и служат причиной изменения размеров и формы деталей при механической обработке древесины. Остаточные напряжения снимают путем дополнительной обработки пиломатериалов (пароувлажнение).
+		</p>
+		<h3>
+			Коробление.
+		</h3>
+		<p>
+			При высыхании или увлажнении древесины изменяется форма поперечного сечения доски. Такое изменение формы называется короблением. Коробление может быть поперечным и продольным. Поперечное выражается в изменении формы сечения доски. Происходит это из-за разницы усушки по радиальному и тангентальному направлениям. Сердцевинные доски уменьшают свои размеры к кромкам: доски, у которых внешняя часть расположена ближе к тангентальному направлению, усыхают больше, чем внутренние, имеющие радиальное направление. Чем ближе доска расположена к сердцевине, тем больше ее коробление.
+			По длине доски могут изгибаться, принимая дугообразную форму или форму винтовой поверхности (крыловатость). Первый вид продольного коробления встречается у досок, содержащих ядро и заболонь (усушка ядра и заболони по длине волокон несколько различается). Крыловатость наблюдается у пиломатериалов с тангентальным наклоном волокон. Для предупреждения появления коробления необходима правильная укладка, сушка и хранение древесины.
+		</p>
+		<h3>
+			Разбухание.
+		</h3>
+		<p>
+			Разбуханием называется увеличение линейных размеров и объема древесины при повышении содержания связанной влаги. Разбухание наблюдается при увеличении влажности до предела гигроскопичности; увеличение свободной влаги не вызывает разбухания. Так же, как и усушка, наибольшее разбухание древесины наблюдается в тангенциальном направлении поперёк волокон, а наименьшее - вдоль волокон. 
+		</p>
+		<h3>
+			Водопоглощение.
+		</h3>
+		<p>
+			Водопоглощение - способность древесины благодаря пористому строению поглощать капельно-жидкую влагу. Водопоглощение происходит при непосредственном контакте древесины с водой. При этом в древесине увеличивается содержание как связанной, так и свободной влаги.
+		</p>
+		<h3>
+			Плотность древесины
+		</h3>
+		<p>
+			Плотность древесины зависит от влажности и для сравнения значения плотности всегда приводят к единой влажности - 12%. 
+			Между плотностью и прочностью древесины существует тесная связь. Более тяжелая древесина, как правило, является более прочной.
+			Величина плотности колеблется в очень широких пределах. По плотности при влажности 12% древесину можно разделить на три группы:
+			- породы с малой плотностью (510 кг/м3 и менее): сосна, ель, пихта, кедр, тополь, липа, ива, ольха, каштан, орех; 
+			- породы средней плотности (550...740 кг/м3 ): лиственница, тис, береза, бук, вяз, груша, дуб, ильм, карагач, клен, платан, рябина, яблоня, ясень; - породы с высокой плотностью (750 кг/м3 и выше): акация белая, береза железная, граб, самшит, саксаул, фисташка, кизил. 
+		</p>
+		<strong>
+			<i>
+				Плотность древесины (г/см3)
+			</i>
+		</strong>
+		<table class="table table-bordered">
+			<caption>
+				<i>
+					Таблица 4.
+				</i>
+			</caption>
+			<tbody>
+				<tr>
+					<th>Бальса</th>
+					<th class="text-center">0.15</th>
+				</tr>
+				<tr>
+					<td>Пихта сибирская</td>
+					<td class="text-center">0.39</td>
+				</tr>
+				<tr>
+					<td>Секвойя вечнозеленая</td>
+					<td class="text-center">0.41</td>
+				</tr>
+				<tr>
+					<td>Ель</td>
+					<td class="text-center">0.45</td>
+				</tr>
+				<tr>
+					<td>Ива</td>
+					<td class="text-center">0.46</td>
+				</tr>
+				<tr>
+					<td>Ольха</td>
+					<td class="text-center">0.49</td>
+				</tr>
+				<tr>
+					<td>Осина</td>
+					<td class="text-center">0.51</td>
+				</tr>
+				<tr>
+					<td>Сосна</td>
+					<td class="text-center">0.52</td>
+				</tr>
+				<tr>
+					<td>Липа</td>
+					<td class="text-center">0.53</td>
+				</tr>
+				<tr>
+					<td>Красное дерево</td>
+					<td class="text-center">0.54</td>
+				</tr>
+				<tr>
+					<td>Конский каштан</td>
+					<td class="text-center">0.56</td>
+				</tr>
+				<tr>
+					<td>Каштан съедобный</td>
+					<td class="text-center">0.59</td>
+				</tr>
+				<tr>
+					<td>Кипарис</td>
+					<td class="text-center">0.60</td>
+				</tr>
+				<tr>
+					<td>Черемуха</td>
+					<td class="text-center">0.61</td>
+				</tr>
+				<tr>
+					<td>Сапелли</td>
+					<td class="text-center">0.62</td>
+				</tr>
+				<tr>
+					<td>Лещина</td>
+					<td class="text-center">0.63</td>
+				</tr>
+				<tr>
+					<td>Орех грецкий</td>
+					<td class="text-center">0.64</td>
+				</tr>
+				<tr>
+					<td>Береза</td>
+					<td class="text-center">0.65</td>
+				</tr>
+				<tr>
+					<td>Вишня</td>
+					<td class="text-center">0.66</td>
+				</tr>
+				<tr>
+					<td>Вяз гладкий</td>
+					<td class="text-center">0.66</td>
+				</tr>
+				<tr>
+					<td>Лиственница</td>
+					<td class="text-center">0.66</td>
+				</tr>
+				<tr>
+					<td>Клен полевой</td>
+					<td class="text-center">0.67</td>
+				</tr>
+				<tr>
+					<td>Тиковое дерево</td>
+					<td class="text-center">0.67</td>
+				</tr>
+				<tr>
+					<td>Бук</td>
+					<td class="text-center">0.68</td>
+				</tr>
+				<tr>
+					<td>Груша</td>
+					<td class="text-center">0.69</td>
+				</tr>
+				<tr>
+					<td>Дуб</td>
+					<td class="text-center">0.69</td>
+				</tr>
+				<tr>
+					<td>Афрормозия</td>
+					<td class="text-center">0.70</td>
+				</tr>
+				<tr>
+					<td>Свитения (махагони)</td>
+					<td class="text-center">0.70</td>
+				</tr>
+				<tr>
+					<td>Платан</td>
+					<td class="text-center">0.70</td>
+				</tr>
+				<tr>
+					<td>Жостер (крушина)</td>
+					<td class="text-center">0.71</td>
+				</tr>
+				<tr>
+					<td>Граб</td>
+					<td class="text-center">0.75</td>
+				</tr>
+				<tr>
+					<td>Падук</td>
+					<td class="text-center">0.75</td>
+				</tr>
+				<tr>
+					<td>Тисс</td>
+					<td class="text-center">0.75</td>
+				</tr>
+				<tr>
+					<td>Ясень</td>
+					<td class="text-center">0.75</td>
+				</tr>
+				<tr>
+					<td>Дуссия</td>
+					<td class="text-center">0.80</td>
+				</tr>
+				<tr>
+					<td>Кемпас</td>
+					<td class="text-center">0.80</td>
+				</tr>
+				<tr>
+					<td>Слива</td>
+					<td class="text-center">0.80</td>
+				</tr>
+				<tr>
+					<td>Сирень</td>
+					<td class="text-center">0.80</td>
+				</tr>
+				<tr>
+					<td>Ятоба (мареил)</td>
+					<td class="text-center">0.80</td>
+				</tr>
+				<tr>
+					<td>Кулахи</td>
+					<td class="text-center">0.83</td>
+				</tr>
+				<tr>
+					<td>Мутения</td>
+					<td class="text-center">0.83</td>
+				</tr>
+				<tr>
+					<td>Палисандр</td>
+					<td class="text-center">0.83</td>
+				</tr>
+				<tr>
+					<td>Венге</td>
+					<td class="text-center">0.84</td>
+				</tr>
+				<tr>
+					<td>Лапачо</td>
+					<td class="text-center">0.84</td>
+				</tr>
+				<tr>
+					<td>Олива</td>
+					<td class="text-center">0.85</td>
+				</tr>
+				<tr>
+					<td>Сандаловое дерево</td>
+					<td class="text-center">0.85</td>
+				</tr>
+				<tr>
+					<td>Панга-панга</td>
+					<td class="text-center">0.85</td>
+				</tr>
+				<tr>
+					<td>Самшит</td>
+					<td class="text-center">0.90</td>
+				</tr>
+				<tr>
+					<td>Лим</td>
+					<td class="text-center">0.90</td>
+				</tr>
+				<tr>
+					<td>Палисандр</td>
+					<td class="text-center">0.90</td>
+				</tr>
+				<tr>
+					<td>Сукупира</td>
+					<td class="text-center">0.90</td>
+				</tr>
+				<tr>
+					<td>Кумару</td>
+					<td class="text-center">0.95</td>
+				</tr>
+				<tr>
+					<td>Хурма эбеновая</td>
+					<td class="text-center">0.96</td>
+				</tr>
+				<tr>
+					<td>Черное дерево</td>
+					<td class="text-center">0.97</td>
+				</tr>
+				<tr>
+					<td>Квебрахо</td>
+					<td class="text-center">1.00</td>
+				</tr>
+				<tr>
+					<td>Гваякум или бакаут</td>
+					<td class="text-center">1.00</td>
+				</tr>
+				<tr>
+					<td>Кумару</td>
+					<td class="text-center">1.10</td>
+				</tr>
+				<tr>
+					<td>Хурма эбеновая</td>
+					<td class="text-center">1.08</td>
+				</tr>
+				<tr>
+					<td>Черное дерево</td>
+					<td class="text-center">1.16</td>
+				</tr>
+				<tr>
+					<td>Квебрахо</td>
+					<td class="text-center">1.21</td>
+				</tr>
+				<tr>
+					<td>Гваякум или бакаут</td>
+					<td class="text-center">1.28</td>
+				</tr>
+			</tbody>
+		</table>
+		<h3>
+			Теплопроводность
+		</h3>
+		<p>
+			 - это способность толщи древесины проводить тепло от одной поверхности к противоположной. Для древесины характерен низкий коэффициент теплопроводности 0,17-0,31 Вт/ (моС), зависящий от породы, плотности, влажности и направления разреза. Сухая древесина плохой проводник тепла.
+		</p>
+		<h3>
+			Звукопроводность
+		</h3>
+		<p>
+			 - это способность древесины проводить звук. Звукопроводность древесины вдоль волокон больше звукопроводности воздуха в 16 раз, а поперек волокон - в 3-4 раза.
+			Качество древесины определяется звукопроводностью. После удара по комлевой части растущего или срубленного ствола хорошее распространение звука свидетельствует о качестве древесины. Прерывистый звук, переходящий в глухой, свидетельствует о загнивании древесины.
+		</p>
+		<h3>
+			Скорость распространения звука в древесине 
+		</h3>
+		<table class="table table-bordered">
+			<caption>
+				Таблица 5.
+			</caption>
+			<thead>
+				<tr>
+					<th>Направление</th>
+					<th class="text-center">Сосна</th>
+					<th class="text-center">Береза</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Вдоль волокон</td>
+					<td class="text-center">5030</td>
+					<td class="text-center">3625</td>
+				</tr>
+				<tr>
+					<td>В радиальном направлении</td>
+					<td class="text-center">1450</td>
+					<td class="text-center">1995</td>
+				</tr>
+				<tr>
+					<td>В тангенциальном направлении</td>
+					<td class="text-center">850</td>
+					<td class="text-center">1535</td>
 				</tr>
 			</tbody>
 		</table>
