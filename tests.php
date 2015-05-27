061dd6430174e7698fdc883f0194c1338988b272
Andrew_Dyachenko <cccp.world@gmail.com>
2015-05-28 02:23:31 +0600

    Общий тест

--- tests.php
+++ tests.php
@@ -29,28 +29,241 @@
 				Обобщающий тест:
 			</h2>
 			<form onsubmit="javascript: void(0);">
-				<div class="form-group">
-					<label>
-						1.К физическим свойствам древесины относятся:
-					</label>
-					<div class="radio">
-						<label>
-							<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-							Option one is this and that&mdash;be sure to include why it's great
-						</label>
-					</div>
-					<div class="radio">
-						<label>
-							<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-							Option two can be something else and selecting it will deselect option one
-						</label>
-					</div>
-					<div class="radio disabled">
-						<label>
-							<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
-							Option three is disabled
-						</label>
-					</div>
+				
+				<ol>
+					<li>
+						<strong>
+							К физическим свойствам древесины относятся:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Плотность, влажность, цвет, запах
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Плотность, твердость, прочность, запах
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Твёрдость, прочность, упругость
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							Способность сопротивляться проникновению в древесину других более твёрдых тел, называют:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Твёрдостью
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Плотностью
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Упругостью
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							К конструкторской документации относятся:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Чертежи, планы, схемы, технологические карты
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Чертежи, эскизы, технические рисунки, расчёты, схемы
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Расчёты, схемы, маршрутные и операционные карты
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							Часть производственного процесса, содержащая действия по изменению формы, размеров и свойств материала при изготовлении изделия, называют:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Технологическая операция
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Технологический переход
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Технологический процесс
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							При подготовке инструмента к работе, на абразивных брусках и оселках лезвие дереворежущего инструмента:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Затачивают
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Правят и доводят
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Полируют
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							У рубанка вылет режущей кромки над подошвой должен составлять:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) 1-2 мм
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) до 5 мм
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) 0,3- 0,5 мм
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							Столярное соединение, у которого соединяемые детали конструктивно одинаковы, называется:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Соединение в полдерева
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Шиповое соединение
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Соединение "ласточкин хвост"
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							При изготовлении шипового соединения используют инструмент:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Ножовка, рашпиль, стамеска
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Ножовка, долото, киянка, стамеска
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Ножовка, рубанок, молоток 
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							Абрамцево-кудринская резьба относится к:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Скульптурной резьбе
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Геометрической резьбе
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Плоскорельефной резьбе
+							</label>
+						</div>
+					</li>
+					<li>
+						<strong>
+							Основу композиции Абрамцево-кудринской резьбы составляет:
+						</strong>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+								А) Растительный и животный орнамент 
+							</label>
+						</div>
+						<div class="radio">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								Б) Геометрический орнамент
+							</label>
+						</div>
+						<div class="radio disabled">
+							<label>
+								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+								В) Геральдический орнамент
+							</label>
+						</div>
+					</li>
+				</ol>
+				<div class="text-center">
+					<button type="button" class="btn btn-primary">Сдать тест на проверку</button>
 				</div>
 			</form>
 		</div>
