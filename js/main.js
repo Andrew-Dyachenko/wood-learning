62ff05ae9869b6e33e62e9cbd18f9c5164a0c3d5
Andrew_Dyachenko <cccp.world@gmail.com>
2015-05-31 20:29:19 +0600

    Общий тест полностью готов.

--- js/main.js
+++ js/main.js
@@ -33,6 +33,71 @@ $(document).ready(function () {
 	setTimeout(function () {
 		$('.banner .text small').addClass('active');
 	}, 800);
-
-	
+});
+var commonTest = {
+	init: function () {
+		var self = this;
+		this.form = $('#commonTest form');
+		this.submit = this.form.find('button');
+		this.inputs = this.form.find('input:radio');
+		this.inputGroups = [];
+		this.form.find('li').each(function (index, element) {
+			self.inputGroups.push($(this).find('input:radio'));
+			self.inputs.on('click', function () {
+				if (self.isReady()) {
+					self.submit.removeAttr('disabled');
+				}
+			});
+		});
+	},
+	isReady: function () {
+		for (var i = 0; i < this.inputGroups.length; i++) {
+			var z = 0;
+			for (var t = 0; t < this.inputGroups[i].length; t++) {
+				if ($(this.inputGroups[i][t]).prop('checked')) z++;
+			}
+			if (!z) {
+				console.debug('false');
+				return false;
+			}
+		}
+		console.debug('true');
+		return true;
+	},
+	checkTest: function () {
+		this.answers = ['1A', '2A', '3B', '4C', '5B', '6C', '7A', '8B', '9C', '10A'];
+		function check (element, index, array) {
+			return $('#' + element).prop('checked');
+		}
+		if (this.answers.every(check))
+			$('#commonTestSuccess').modal('show');
+		else
+			$('#commonTestFailed').modal('show');
+	},
+	reset: function () {
+		this.inputs.each(function () {
+			$(this).attr('checked', false);
+		});
+		this.submit.attr('disabled', true);
+	}
+}
+/**
+ * Vertically center Bootstrap 3 modals so they aren't always stuck at the top
+ */
+$(function() {
+    function reposition() {
+        var modal = $(this),
+            dialog = modal.find('.modal-dialog');
+        modal.css('display', 'block');
+        
+        // Dividing by two centers the modal exactly, but dividing by three 
+        // or four works better for larger screens.
+        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+    }
+    // Reposition when a modal is shown
+    $('.modal').on('show.bs.modal', reposition);
+    // Reposition when the window is resized
+    $(window).on('resize', function() {
+        $('.modal:visible').each(reposition);
+    });
 });
--- tests.php
+++ tests.php
@@ -8,273 +8,350 @@
 		</h1>
 		<div role="tabpanel">
 
-	<!-- Nav tabs -->
-	<ul class="nav nav-tabs" role="tablist">
-		<li role="presentation" class="active">
-			<a href="#commonTest" aria-controls="home" role="tab" data-toggle="tab">
-				Общий тест
-			</a>
-		</li>
-		<li role="presentation">
-			<a href="#physicalAndMechanical" aria-controls="profile" role="tab" data-toggle="tab">
-				Физико-механический тест
-			</a>
-		</li>
-	</ul>
+			<!-- Nav tabs -->
+			<ul class="nav nav-tabs" role="tablist">
+				<li role="presentation" class="active">
+					<a href="#commonTest" aria-controls="commonTest" role="tab" data-toggle="tab">
+						Общий тест
+					</a>
+				</li>
+				<li role="presentation">
+					<a href="#physicalAndMechanical" aria-controls="physicalAndMechanical" role="tab" data-toggle="tab">
+						Физико-механический тест
+					</a>
+				</li>
+			</ul>
 
-	<!-- Tab panes -->
-	<div class="tab-content">
-		<div role="tabpanel" class="tab-pane fade in active" id="commonTest">
-			<h2 class="text-center">
-				Обобщающий тест:
-			</h2>
-			<form onsubmit="javascript: void(0);">
-				
-				<ol>
-					<li>
-						<strong>
-							К физическим свойствам древесины относятся:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Плотность, влажность, цвет, запах
-							</label>
+			<!-- Tab panes -->
+			<div class="tab-content">
+				<div role="tabpanel" class="tab-pane fade in active" id="commonTest">
+					<h2 class="text-center">
+						Обобщающий тест:
+					</h2>
+					<form onsubmit="javascript: void(0);">
+						<ol>
+							<li>
+								<strong>
+									К физическим свойствам древесины относятся:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios1" id="1A" value="option1">
+										А) Плотность, влажность, цвет, запах
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios1" id="1B" value="option2">
+										Б) Плотность, твердость, прочность, запах
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios1" id="1C" value="option3">
+										В) Твёрдость, прочность, упругость
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									Способность сопротивляться проникновению в древесину других более твёрдых тел, называют:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios2" id="2A" value="option1">
+										А) Твёрдостью
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios2" id="2B" value="option2">
+										Б) Плотностью
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios2" id="2C" value="option3">
+										В) Упругостью
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									К конструкторской документации относятся:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios3" id="3A" value="option1">
+										А) Чертежи, планы, схемы, технологические карты
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios3" id="3B" value="option2">
+										Б) Чертежи, эскизы, технические рисунки, расчёты, схемы
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios3" id="3C" value="option3">
+										В) Расчёты, схемы, маршрутные и операционные карты
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									Часть производственного процесса, содержащая действия по изменению формы, размеров и свойств материала при изготовлении изделия, называют:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios4" id="4A" value="option1">
+										А) Технологическая операция
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios4" id="4B" value="option2">
+										Б) Технологический переход
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios4" id="4C" value="option3">
+										В) Технологический процесс
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									При подготовке инструмента к работе, на абразивных брусках и оселках лезвие дереворежущего инструмента:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios5" id="5A" value="option1">
+										А) Затачивают
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios5" id="5B" value="option2">
+										Б) Правят и доводят
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios5" id="5C" value="option3">
+										В) Полируют
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									У рубанка вылет режущей кромки над подошвой должен составлять:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios6" id="6A" value="option1">
+										А) 1-2 мм
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios6" id="6B" value="option2">
+										Б) до 5 мм
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios6" id="6C" value="option3">
+										В) 0,3- 0,5 мм
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									Столярное соединение, у которого соединяемые детали конструктивно одинаковы, называется:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios7" id="7A" value="option1">
+										А) Соединение в полдерева
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios7" id="7B" value="option2">
+										Б) Шиповое соединение
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios7" id="7C" value="option3">
+										В) Соединение "ласточкин хвост"
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									При изготовлении шипового соединения используют инструмент:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios8" id="8A" value="option1">
+										А) Ножовка, рашпиль, стамеска
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios8" id="8B" value="option2">
+										Б) Ножовка, долото, киянка, стамеска
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios8" id="8C" value="option3">
+										В) Ножовка, рубанок, молоток 
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									Абрамцево-кудринская резьба относится к:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios9" id="9A" value="option1">
+										А) Скульптурной резьбе
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios9" id="9B" value="option2">
+										Б) Геометрической резьбе
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios9" id="9C" value="option3">
+										В) Плоскорельефной резьбе
+									</label>
+								</div>
+							</li>
+							<li>
+								<strong>
+									Основу композиции Абрамцево-кудринской резьбы составляет:
+								</strong>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios10" id="10A" value="option1">
+										А) Растительный и животный орнамент 
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios10" id="10B" value="option2">
+										Б) Геометрический орнамент
+									</label>
+								</div>
+								<div class="radio">
+									<label>
+										<input type="radio" name="commonRadios10" id="10C" value="option3">
+										В) Геральдический орнамент
+									</label>
+								</div>
+							</li>
+						</ol>
+						<div class="text-center">
+							<button type="button" onclick="javascript: commonTest.checkTest();" class="btn btn-primary" disabled="disabled">
+								Сдать тест на проверку
+							</button>
 						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Плотность, твердость, прочность, запах
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Твёрдость, прочность, упругость
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							Способность сопротивляться проникновению в древесину других более твёрдых тел, называют:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Твёрдостью
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Плотностью
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Упругостью
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							К конструкторской документации относятся:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Чертежи, планы, схемы, технологические карты
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Чертежи, эскизы, технические рисунки, расчёты, схемы
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Расчёты, схемы, маршрутные и операционные карты
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							Часть производственного процесса, содержащая действия по изменению формы, размеров и свойств материала при изготовлении изделия, называют:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Технологическая операция
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Технологический переход
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Технологический процесс
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							При подготовке инструмента к работе, на абразивных брусках и оселках лезвие дереворежущего инструмента:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Затачивают
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Правят и доводят
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Полируют
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							У рубанка вылет режущей кромки над подошвой должен составлять:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) 1-2 мм
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) до 5 мм
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) 0,3- 0,5 мм
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							Столярное соединение, у которого соединяемые детали конструктивно одинаковы, называется:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Соединение в полдерева
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Шиповое соединение
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Соединение "ласточкин хвост"
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							При изготовлении шипового соединения используют инструмент:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Ножовка, рашпиль, стамеска
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Ножовка, долото, киянка, стамеска
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Ножовка, рубанок, молоток 
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							Абрамцево-кудринская резьба относится к:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Скульптурной резьбе
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Геометрической резьбе
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Плоскорельефной резьбе
-							</label>
-						</div>
-					</li>
-					<li>
-						<strong>
-							Основу композиции Абрамцево-кудринской резьбы составляет:
-						</strong>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-								А) Растительный и животный орнамент 
-							</label>
-						</div>
-						<div class="radio">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-								Б) Геометрический орнамент
-							</label>
-						</div>
-						<div class="radio disabled">
-							<label>
-								<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
-								В) Геральдический орнамент
-							</label>
-						</div>
-					</li>
-				</ol>
-				<div class="text-center">
-					<button type="button" class="btn btn-primary">Сдать тест на проверку</button>
+					</form>
+					<script>
+						$(document).ready(function () {
+							commonTest.init();
+						});
+					</script>
 				</div>
-			</form>
+				<div role="tabpanel" class="tab-pane fade" id="physicalAndMechanical">
+					<h2 class="text-center">
+						Физико-механические свойства древесины:
+					</h2>
+					<form onsubmit="javascript: void(0);"></form>
+				</div>
+			</div>
 		</div>
-		<div role="tabpanel" class="tab-pane fade" id="physicalAndMechanical">
-			<h2 class="text-center">
-				Физико-механические свойства древесины:
-			</h2>
-			<form onsubmit="javascript: void(0);"></form>
+		<!-- Common fail modal -->
+		<div class="modal fade" id="commonTestFailed" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">
+								&times;
+							</span>
+						</button>
+						<h4 class="modal-title" id="myModalLabel">
+							Тест провален
+						</h4>
+					</div>
+					<div class="modal-body">
+						<div class="image text-center text-danger">
+							<strong>
+								&times;
+							</strong>
+						</div>
+						<p class="text-center">
+							К сожалению не все ответы оказались верными
+						</p>
+					</div>
+					<div class="modal-footer">
+						<div class="text-left inline">
+							Хотите пройти снова?
+						</div>
+						<button type="button" class="btn btn-default" data-dismiss="modal">
+							Нет
+						</button>
+						<button type="button" class="btn btn-warning" data-dismiss="modal" onclick="javascript: commonTest.reset();">
+							Да
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
+
+		<!-- Common success modal -->
+		<div class="modal fade" id="commonTestSuccess" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">
+								&times;
+							</span>
+						</button>
+						<h4 class="modal-title" id="myModalLabel">
+							Тест пройден
+						</h4>
+					</div>
+					<div class="modal-body">
+						<div class="image text-center text-success">
+							<strong>
+								&#10003;
+							</strong>
+						</div>
+						<p class="text-center">
+							Вы ответили верно на все пункты.
+						</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-success" data-dismiss="modal">
+							Ок
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 </div>
 <?php include 'footer.php'; ?>
